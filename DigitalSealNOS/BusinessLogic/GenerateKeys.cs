dc80204fcb240f01c520a683d5a8fd55fc0a09ee
tpapic21 <tpapic21@foi.hr>
2024-11-23 17:34:22 +0100

    Add: Option to generate public, private and secret keys

---
+++ DigitalSealNOS/BusinessLogic/GenerateKeys.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DigitalSealNOS.BusinessLogic
+{
+    public class GenerateKeys
+    {
+        private readonly string symmetricKeyPath = "tajni_kljuc.txt";
+        private readonly string publicKeyPath = "javni_kljuc.txt";
+        private readonly string privateKeyPath = "privatni_kljuc.txt";
+
+        private void Generate()
+        {
+            try
+            {
+                GenerateSymmetricKey();
+                GenerateAsymmetricKeys();
+            }
+            catch (Exception ex)
+            {
+                // Log the exception (consider using a logging framework)
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private void GenerateSymmetricKey()
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.GenerateKey();
+                File.WriteAllBytes(symmetricKeyPath, aes.Key);
+            }
+        }
+
+        private void GenerateAsymmetricKeys()
+        {
+            using (RSA rsa = RSA.Create())
+            {
+                
+                string publicKey = Convert.ToBase64String(rsa.ExportRSAPublicKey());
+                File.WriteAllText(publicKeyPath, publicKey);
+
+           
+                string privateKey = Convert.ToBase64String(rsa.ExportRSAPrivateKey());
+                File.WriteAllText(privateKeyPath, privateKey);
+            }
+        }
+
+        public void GenerateAndStoreKeys()
+        {
+            Generate();
+        }
+    }
+}
--- DigitalSealNOS/Presentation/Form1.cs
+++ DigitalSealNOS/Presentation/Form1.cs
@@ -1,19 +1,32 @@
-using DigitalSealNOS.BusinessLogic;
+﻿using DigitalSealNOS.BusinessLogic;
 
 namespace DigitalSealNOS
 {
     public partial class Form1 : Form
     {
-
+        private GenerateKeys generateKeys;
         public Form1()
         {
             InitializeComponent();
+            InitClassObjects();
+        }
 
+        private void InitClassObjects()
+        {
+            generateKeys = new();
         }
 
         private void btnGenerateKeys_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                generateKeys.GenerateAndStoreKeys();
+                MessageBox.Show("Ključevi uspješno generirani!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
         }
 
         private void btnEncryptFile_Click(object sender, EventArgs e)
